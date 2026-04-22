74ceb6d97c91ab430ecae668fb9017a6a075898d
Christopher Klose <christopherklose94@gmail.com>
2026-04-22 15:56:41 +0200

    Update the init file of the material database

--- src/scattering_calculator/database/__init__.py
+++ src/scattering_calculator/database/__init__.py
@@ -1,10 +1,5 @@
 """Material databases for coherent scattering simulations."""
 
 from . import material_parameter
-from .material_parameter import load_l_edge, load_m_edge
 
-__all__ = [
-    "load_l_edge",
-    "load_m_edge",
-    "material_parameter",
-]
+__all__ = ["material_parameter", "load_l_edge", "load_m_edge"]
