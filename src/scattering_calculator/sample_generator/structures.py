2ae68df549600c48895ffadcea2563cf7ee2ab0e
riccbatt <ricc.batt94@gmail.com>
2026-04-23 18:13:02 +0200

    progress in M and C definition

--- src/scattering_calculator/sample_generator/structures.py
+++ src/scattering_calculator/sample_generator/structures.py
@@ -1210,13 +1210,12 @@ class Apertures3D:
         """
         if use_real_space_coordinates:
             # Convert radius from metres to pixels using the real-space grid
-            pixel_radius = radius / np.abs(self.x[0,0, 1] - self.x[0,0, 0])
+            pixel_radius = radius / np.abs(self.x[0,1,0] - self.x[0,0, 0])
             pixel_depth = np.argmin(np.abs(np.append(0, self.layer_thicknesses) - depth))
         else:
             pixel_radius = radius
             pixel_depth = radius
 
-        print(pixel_radius,np.abs(self.x[0,0, 1] - self.x[0,0, 0]))
         self.aperture_design = np.ones(self.shape)
 
 
